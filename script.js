4d2039acaf8ce0b5547430fc6b0a6cae0ced261c
Ricardo Salazar <no-replay>
2021-05-23 11:17:23 -0500

    erased everything and started over add html and variables for quiz

---
+++ script.js
@@ -0,0 +1,42 @@
+// variables for the quiz//
+
+var pos= 0, quiz, quiz_status, question, choice, choices, cha, chb, chc, chd, correct = 0;
+
+var questions = [
+{
+      question: "What does HTML stand for?",
+      a:"Hypertrophic Management Language",
+      b:"Hypertext Main Language",
+      c:"Hypertext Markup Language",
+      answer:"c"
+    },
+{
+      question: "What does CSS stand for?",
+      a:"Cascading Style Software",
+      b:"Cascading Style Sheets",
+      c:"Customize Style Sheets",
+      answer:"b"
+    },  
+{
+      question: "What is JavaScript",
+      a:"Is a Programing Languge",
+      b:"Is a Computer Game",
+      c:"Is a web application",
+      answer:"a"
+    },
+{
+      question: "What is the main use of HTML?",
+      a:"Add style to a web page",
+      b:"Structure a web page and its content",
+      c:"Enable the funcionality of a web page",
+      answer:"b"
+    },
+{
+      question: "What does JavaScript do?",
+      a:"Give you instructions for web searching",
+      b:"Gives web pages interactive elements",
+      c:"Allow the writing of files on server machines",
+      answer:"b"
+    }  
+];  
+
